d241f5b747f23cc64c906cd27e93fb70e122f2db
Chukwu3meka <chukwuemeka@alienforest.com>
2022-11-23 13:58:09 +0100

    Image Update
    Used real photo for intro and added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -58,6 +58,11 @@ const App = ({ Component, pageProps }) => {
         <meta property="twitter:url" content="https://www.twitter.com/maduekwepedro" />
         <meta property="twitter:description" content="Full stack Developer" />
         <meta property="twitter:card" content="summary_large_image" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ThemeProvider theme={muiTheme}>
         <SnackbarProvider maxSnack={1} preventDuplicate>
