b55bb809b430e57550de20befb92e2a5d62dd116
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-20 01:28:47 +0100

    fixed deployment error

--- pages/_document.js
+++ pages/_document.js
@@ -13,11 +13,8 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Amiri:ital@1&amp;family=Cookie&amp;family=Creepster&amp;display=swap"
-            rel="stylesheet"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Amiri:ital@1&amp;family=Cookie&amp;family=Creepster&amp;display=swap" rel="stylesheet" />
         </Head>
         <body style={{ margin: 0 }}>
           <Main />
